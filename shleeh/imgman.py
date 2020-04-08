f1d666e100ee7c57c813d1bea355df988090099d
dvm-shlee <shlee@unc.edu>
2020-04-07 22:20:30 -0400

    image ras re-orientation method added

--- shleeh/imgman.py
+++ shleeh/imgman.py
@@ -2,22 +2,27 @@ import numpy as np
 from scipy.ndimage import affine_transform
 
 
-def reorient_to_ras(data, affine, vox_resol):
+def reorient_to_ras(data, affine, resol):
     """ Reorient and re-sample the input data into RAS space
     to ensure consistent orientation regardless of which axis
     was sliced during data acquisition.
+
+    Returns:
+        ras_data
+        ras_resol
     """
-    org_shape = data.shape
-    fov_size = np.asarray(org_shape) * vox_resol
-    rotate_mat = (affine[:3, :3] / vox_resol).astype(np.int8)
+    shape = data.shape
+    fov_size = np.asarray(shape) * resol
+    rotate_mat = (affine[:3, :3] / resol).astype(np.int8)
     origin = rotate_mat.dot(fov_size/2)
 
     org_affine = affine.copy()
     org_affine[:3, 3] = -origin
 
-    ras_shape = abs(rotate_mat.dot(org_shape))
+    ras_resol = abs(rotate_mat.dot(resol))
+    ras_shape = abs(rotate_mat.dot(shape))
     ras_affine = np.eye(4)
-    ras_affine[:3, :3] = np.diag(vox_resol)
+    ras_affine[:3, :3] = np.diag(resol)
     ras_affine[:3, 3] = -fov_size/2
 
     org_mm2vox = np.linalg.inv(org_affine)
@@ -25,4 +30,13 @@ def reorient_to_ras(data, affine, vox_resol):
 
     rotate = ras_mm2vox[:3, :3]
     shift = ras_mm2vox[:3, 3]
-    return affine_transform(data, rotate, shift, output_shape=ras_shape)
+
+    ras_data = affine_transform(data, rotate, shift, output_shape=ras_shape)
+    return ras_data, ras_resol
+
+
+def determine_slice_planes(slice_axis, affine, resol):
+    """ return the original scheme of slice plane """
+    rotate_mat = (affine[:3, :3] / resol).astype(np.int8)
+    ras_axis = abs(rotate_mat.dot(range(3)).tolist())
+    return ['sagittal', 'axial', 'coronal'][ras_axis.index(slice_axis)]
