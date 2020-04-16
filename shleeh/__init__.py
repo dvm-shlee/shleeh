443eb8441a1361ad0d6d2f3ca571ed1ca39a0bae
SungHo Lee <shlee@unc.edu>
2020-04-16 14:56:11 -0400

    0.3.0.b4 bids tool added

---
+++ shleeh/__init__.py
@@ -0,0 +1,18 @@
+from .enums import *
+from .errors import *
+from .utils import *
+
+__version__ = '0.0.2'
+
+__all__ = ['__version__',
+           'TimeCounter',
+           'DataType',
+           'Error',
+           'FileNotValidError',
+           'ArchiveFailedError',
+           'RemoveFailedError',
+           'RenameFailedError',
+           'UnexpectedError',
+           'InvalidValueInField',
+           'ValueConflictInField'
+           ]
