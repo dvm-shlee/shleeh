f824380f6c790a6b5c28b58abe7ff75841f452a1
SungHo Lee <shlee@unc.edu>
2020-04-16 15:01:33 -0400

    remove excluded data

--- build/lib/shleeh/__init__.py
+++
@@ -1,9 +0,0 @@
-from .enums import *
-from .errors import *
-
-__version__ = '0.0.1.dev1'
-
-__all__ = ['__version__',
-           'DataType',
-           'Error',
-           'FileNotValidError', ]
